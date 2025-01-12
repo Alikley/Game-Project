2a52ec612f248338c2c33ffd8123368edcc0e95e
Alikley <alikley933@gmail.com>
2025-01-12 15:20:45 +0330

    new

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
 import GameDetaillPage from "./pages/GameDetaillPage";
+import ErorrPage from "./pages/ErorrPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErorrPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "games/:id", element: <GameDetaillPage /> },
