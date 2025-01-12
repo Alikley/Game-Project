ca57006cd44a96df4812e423e5dc08b86ed5d197
Alikley <alikley933@gmail.com>
2025-01-12 15:14:06 +0330

    new

---
+++ src/pages/GameDetaillPage.tsx
@@ -0,0 +1,14 @@
+import { useParams } from "react-router-dom";
+
+
+function GameDetaillPage() {
+    const param =    useParams()
+  return (
+    <div>
+        <h1>game</h1>
+        
+    </div>
+  )
+}
+
+export default GameDetaillPage
